829b47509659baecc8e69cda1a7306f29df18290
Keegan Neave <keegan@neave.engineering>
2024-01-24 14:25:55 +0000

    updated motor script

--- nefive_motion/src/mecanum_motor_node.py
+++ nefive_motion/src/mecanum_motor_node.py
@@ -4,12 +4,16 @@ import rospy
 import redboard
 import math
 import time
+import threading
 
 from sensor_msgs.msg import Joy
+from ros_redboard.msg import ADC
 
 # time in seconds in not recieving a message before stopping
 msg_timeout = rospy.Duration(0.50)
 last_msg_received = rospy.Time.from_sec(time.time())
+
+pub = None
 
 rb = redboard.RedBoard()
 
@@ -53,27 +57,32 @@ def callback(data):
 
     else:
         control_movement = data.buttons[2] == 1
-        x, y, z = -data.axes[1], data.axes[0], data.axes[2]
+        x, y, z, torso = -data.axes[1], data.axes[0], data.axes[2], data.axes[6]
 
     if(control_movement == True):
         # print(data.axes[0], data.axes[1])
         front_left, front_right, back_left, back_right = steering(x, y, z)
         # print(front_left, front_right, back_left, back_right)
 
+        base_motor_speed = 0.2
+        turbo_multiplier = 1.25
+
         # Buttons are on when down so this makes sense in the physical world
         if(data.buttons[4] == 1):
             # Low speed, halve values
-            front_left = front_left * 0.33
-            front_right = front_right * 0.33
-            back_left = back_left * 0.33
-            back_right = back_right * 0.33
+            front_left = front_left     * base_motor_speed
+            front_right = front_right   * base_motor_speed
+            back_left = back_left       * base_motor_speed
+            back_right = back_right     * base_motor_speed
         else:
-            front_left = front_left * 0.66
-            front_right = front_right * 0.66
-            back_left = back_left * 0.66
-            back_right = back_right * 0.66
+            front_left = front_left     * base_motor_speed * turbo_multiplier
+            front_right = front_right   * base_motor_speed * turbo_multiplier
+            back_left = back_left       * base_motor_speed * turbo_multiplier
+            back_right = back_right     * base_motor_speed * turbo_multiplier
 
         setmotors(front_left, front_right, back_left, back_right)
+        rb.servo7 = torso
+
     else:
         #  print("Motors not enabled.")
         setmotors(0, 0, 0, 0)
@@ -84,6 +93,25 @@ def setmotors(m0, m1, m2, m3):
     rb.m2 = m2
     rb.m3 = m3
 
+def publish_adc():
+    try:
+        # Loop until disconnected
+        while not rospy.is_shutdown():
+            # Create message
+            msg = ADC()
+            msg.adc0 = rb.adc0
+            msg.adc1 = rb.adc1
+            msg.adc2 = rb.adc2
+            msg.adc3 = rb.adc3
+
+            # rospy.loginfo(msg)
+            pub.publish(msg)
+
+            rospy.sleep(1)
+
+    except rospy.ROSInterruptException:
+        pass
+
 
 def timeout_check(event):
     timediff = rospy.Time.now() - last_msg_received
@@ -92,6 +120,7 @@ def timeout_check(event):
         setmotors(0, 0, 0, 0)
 
 def listener():
+    global pub
     # In ROS, nodes are uniquely named. If two nodes with the same
     # name are launched, the previous one is kicked off. The
     # anonymous=True flag means that rospy will choose a unique
@@ -99,9 +128,13 @@ def listener():
     # run simultaneously.
     rospy.init_node('motor_driver', anonymous=True)
     rospy.Subscriber('joy', Joy, callback)
-
+    pub = rospy.Publisher('adc', ADC, queue_size=10)
+    
     # Set up the timer-based timeout check
     rospy.Timer(msg_timeout, timeout_check)
+    adcThread = threading.Thread(target=publish_adc)
+    adcThread.start()
+    adcThread.join()
 
     rospy.spin()
 
